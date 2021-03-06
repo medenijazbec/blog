f8e7ea0b15c480eb0ee12aba217334e29a06d647
Matic Ozimic <matic.ozimic@gmail.com>
2021-03-06 13:35:13 +0100

    posts+users

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_06_121300) do
+ActiveRecord::Schema.define(version: 2021_03_06_123253) do
 
   create_table "news", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+# Could not dump table "posts" because of following StandardError
+#   Unknown type 'reference' for column 'user_id'
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
