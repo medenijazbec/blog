97c189f73035ec81d09bfce64fcc8e7d7f05dfa0
Matic Ozimic <matic.ozimic@gmail.com>
2021-03-06 17:32:08 +0100

    format helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,5 +5,16 @@ module ApplicationHelper
         html += link_to "Registracija", new_user_registration_path unless user_signed_in?
         html += link_to "Odjava", destroy_user_session_path, method: :delete if user_signed_in?
         html.html_safe
+        def display_session_links
+            capture do
+              if user_signed_in?
+                concat link_to "Odjava", destroy_user_session_path, method: :delete
+              else
+                concat link_to "Prijava", new_user_session_path
+                concat " "
+                concat link_to "Registracija", new_user_registration_path
+                concat " "
+              end
+            end
       end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,6 +4,12 @@ module PostsHelper
         if current_user == post.user
           html += "<td>#{link_to 'Edit', edit_post_path(post)}</td>"
           html += "<td>#{link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }}</td>"
+          capture do
+            concat content_tag(:td, link_to('Show', post))
+            if current_user == post.user
+              concat content_tag(:td, link_to('Edit', edit_post_path(post)))
+              concat content_tag(:td, link_to('Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }))
+            end
         end
         html.html_safe
       end
@@ -13,6 +19,12 @@ module PostsHelper
         if current_user == post.user
           html += link_to 'Edit', edit_post_path(post)
           html += ' | '
+          capture do
+            if current_user == post.user
+              concat link_to 'Edit', edit_post_path(post)
+              concat ' | '
+            end
+            concat link_to 'Back', posts_path
         end
         html += link_to 'Back', posts_path
         html.html_safe
