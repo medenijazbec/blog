8dcabaf800c01dc544732c8b1afbc0b36478bd77
Matic Ozimic <matic.ozimic@gmail.com>
2021-03-06 17:21:38 +0100

    added bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,15 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+require("@rails/ujs").start()
+require("@rails/activestorage").start()
+require("channels")
+// CSS
+import 'scss/site'
+// JS
+import('js/site')
+// Images
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
